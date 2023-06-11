58694a44e18807bff9f670aed20261a52851fa1f
Wojciech Wolosz <wojciech.wolosz@racing.agh.edu.pl>
2023-06-11 21:32:24 +0200

    Added 3.0 version

--- tin_final_project/main.py
+++ tin_final_project/main.py
@@ -6,32 +6,61 @@ SERIAL_INIT_CONFIG = "C\rS8\rO\r"
 SERIAL_PORT = "COM7"
 SERIAL_BAUD_RATE = 1000000
 NUMBER_OF_BYTES = 19
-
+DELAY = 0.5
 
 frame = "t7023010100\r"
 
 my_serial = serial.Serial(port=SERIAL_PORT, baudrate=SERIAL_BAUD_RATE, bytesize=8, parity=serial.PARITY_NONE,
                           stopbits=1,
                           timeout=0.05, xonxoff=True)
+def init():
+    my_serial.write(SERIAL_INIT_CONFIG.encode(encoding='UTF-8'))
+    sleep(DELAY)
 
-my_serial.write(SERIAL_INIT_CONFIG.encode(encoding='UTF-8'))
-sleep(1)
-
-for i in range(5):
-    frame = "t70230" + str(i) + "0100\r"
-    my_serial.write(frame.encode(encoding='UTF-8'))
-    sleep(0.1)
-
-# for i in range(5):
-#     frame = "t70230" + str(i) + "0000\r"
-#     my_serial.write(frame.encode(encoding='UTF-8'))
-#     sleep(1)
-
-
-count = 0
-while(1):
+def test_on():
+    print("Test #1 - turning on the relays")
+    for i in range(0, 5):
+        frame = "t70230" + str(i) + "0100\r"
+        my_serial.write(frame.encode(encoding='UTF-8'))
+        sleep(DELAY)
+    my_serial.readline().decode(encoding='UTF-8').strip()
+    sleep(DELAY)
     s = my_serial.readline().decode(encoding='UTF-8').strip()
-    s = s[0:19]
-    sleep(0.1)
-    print("count #" + str(count) + ": " + s)
-    count = count+1
+    #print("Read status frame:" + s[0:19])
+    if (s[5:7] == "1F"):
+        return 1
+    else:
+        return 0
+
+def test_off():
+    print("Test #2 - turning off the relays")
+    for i in range(0, 5):
+        frame = "t70230" + str(i) + "0000\r"
+        my_serial.write(frame.encode(encoding='UTF-8'))
+        sleep(DELAY)
+    my_serial.readline().decode(encoding='UTF-8').strip()
+    sleep(DELAY)
+    s = my_serial.readline().decode(encoding='UTF-8').strip()
+    #print("Read status frame:" + s[0:19])
+    if (s[5:7] == "00"):
+        return 1
+    else:
+        return 0
+
+
+
+init()
+
+while(1):
+    input("Press any key to launch tests:")
+    if (test_on() == 1):
+        print("All good!")
+    else:
+        print("Shit's fucked!")
+
+    if (test_off() == 1):
+        print("All good!")
+    else:
+        print("Shit's fucked!")
+
+
